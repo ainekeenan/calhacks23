f3ec50dac1730cfb3247535a878ab3c6cd6d21b1
ainekeenan <keenancaine@gmail.com>
2023-10-29 10:27:16 -0700

    styling

--- frontend/src/pages/welcome.js
+++ frontend/src/pages/welcome.js
@@ -4,6 +4,7 @@ const Welcome = () => {
     return(<div> 
     <h1>Welcome to the Feeling Song player</h1>
     <p> Made for CalHacks 2023</p>
+    <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlBrjh8pRduKqB8g6PeJeNZnETx55uCSn58wyqJjxeLfaOHZjKkeYwSMxAtiO8SF9Rr2A&usqp=CAU" alt = "Meet your DJ"></img>
     </div> );
 };
 
